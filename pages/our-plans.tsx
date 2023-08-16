8859f1894c2c5ce6766af2e4548deceddee9c58c
Song Wan Jin <songjin900@gmail.com>
2023-08-15 23:07:36 -0400

    clean up 2

--- pages/our-plans.tsx
+++ pages/our-plans.tsx
@@ -21,14 +21,14 @@ const OurPlan: NextPage<{ cuisine: Cuisine[], goal: Goal[] }> = ({ cuisine, goal
     const [submitType, setSubmitType] = useState("update");
     const [buttonDisabled, setButtonDisabled] = useState(false);
     const [planStep, setPlanStep] = useState(1);
-    const [selectedCuisineIndex, setSelectedCuisineIndex] = useState(Array(8).fill(''));
-    const [selectedReasonIndex, setSelectedReasonIndex] = useState(Array(4).fill(''));
-    const [selectedOrder, setSelectedOrder] = useState([4, 4])
 
     const [selectedCuisine, setSelectedCuisine] = useState<number[]>([]);
     const [selectedGoal, setSelectedGoal] = useState<number[]>([]);
     const peopleNum = [2, 4];
-    const [selectedPeopleNum, setSelectedPeopleNum] = useState<number>();
+    const [selectedPeopleNum, setSelectedPeopleNum] = useState<number>(0);
+    const cuisineNum = [3, 4, 5];
+    const [selectedCuisineNum, setSelectedCuisineNum] = useState<number>(0);
+
 
 
     {/* **********************************************************************************************************************************************************
@@ -54,24 +54,20 @@ const OurPlan: NextPage<{ cuisine: Cuisine[], goal: Goal[] }> = ({ cuisine, goal
         }
     }
 
-    const peopleClicked = (num: number) => {
+    const peopleNumClicked = (num: number) => {
         if (num) {
             setSelectedPeopleNum(num);
         }
     }
 
-    const handlePeopleClicked = (choice) => {
-        const updatedSelectedOrder = [...selectedOrder]
-        updatedSelectedOrder[0] = choice
-        setSelectedOrder(updatedSelectedOrder)
+    const cuisineNumClicked = (num: number) => {
+        if (num) {
+            setSelectedCuisineNum(num);
+        }
     }
-    const handleFoodButtonlicked = (choice) => {
-        const updatedSelectedOrder = [...selectedOrder]
-        updatedSelectedOrder[1] = choice
-        setSelectedOrder(updatedSelectedOrder)
-    }
-   
-  
+
+
+
     {/* **********************************************************************************************************************************************************
 **********************************************************************************************************************************************************
                                                                         Routing
@@ -89,9 +85,7 @@ const OurPlan: NextPage<{ cuisine: Cuisine[], goal: Goal[] }> = ({ cuisine, goal
     }, [router, submitType])
 
     const onPlanStepClicked = () => {
-        if (planStep === 4) {
-            setPlanStep(planStep);
-        } else {
+        if (planStep !== 4) {
             setPlanStep(prev => prev + 1);
         }
     }
@@ -107,7 +101,7 @@ const OurPlan: NextPage<{ cuisine: Cuisine[], goal: Goal[] }> = ({ cuisine, goal
     }, [router, submitType])
 
     return (
-        <Layout hasTabBar title="Purchase">
+        <Layout hasTabBar title="Our Plan">
             {/* mobile & tablet view */}
             <div className={`bg-gray-100 w-full min-h-[40.625rem] items-center justify-center web:hidden`}>
                 <div className={`flex flex-col max-w-3xl items-center justify-center mx-auto bg-white mt-20 ${planStep === 1 ? "" : "hidden"}`}>
@@ -170,17 +164,15 @@ const OurPlan: NextPage<{ cuisine: Cuisine[], goal: Goal[] }> = ({ cuisine, goal
                         subtitle=""
                     />
                     <div className={`flex flex-col w-full text-lg justify-center items-center px-2`}>
-
                         {
                             peopleNum
-                            .map((p) => <button className={`bg-white border-2 h-[4rem] w-64 rounded-sm text-[#3A4884] 
+                                .map((p) => <button className={`bg-white border-2 h-[4rem] w-64 rounded-sm text-[#3A4884] 
                             ${selectedPeopleNum === p ? "font-bold border-2 border-[#3A4884]" : ""}`}
-                                key={p} onClick={() => peopleClicked(p)}>{p}</button>)                            
+                                    key={p} onClick={() => peopleNumClicked(p)}>{p}</button>)
                         }
                         {
                             selectedPeopleNum && selectedPeopleNum === 2 ? <div>This is great for an individual or a pair preparing their meals.</div> : selectedPeopleNum && selectedPeopleNum === 4 ? <div>This is great to for families or friends!</div> : null
                         }
-
                     </div>
                     <div className="grid grid-cols-1 items-start px-4 tablet:grid-cols-2 web:grid-cols-4">
                         <div className="tablet:col-span-2 tablet:px-[130px] web:col-span-4 mt-8 web:mt-10 mb-2 web:px-[225px]">
@@ -201,7 +193,16 @@ const OurPlan: NextPage<{ cuisine: Cuisine[], goal: Goal[] }> = ({ cuisine, goal
                         subtitle=""
                     />
                     <div className={`flex flex-row w-full text-lg gap-3 justify-center items-center px-2`}>
-                        <ChoiceButton onClick={handleFoodButtonlicked} choices={["3", "4", "5"]} choiceTexts={["Our most popular plan", "Ready for next week plan", "A new dinner every night plan"]} />
+                        {/* <ChoiceButton onClick={handleFoodButtonlicked} choices={["3", "4", "5"]} choiceTexts={["Our most popular plan", "Ready for next week plan", "A new dinner every night plan"]} /> */}
+                        {
+                            cuisineNum
+                                .map((p) => <button className={`bg-white border-2 h-[4rem] w-64 rounded-sm text-[#3A4884] 
+                            ${selectedCuisineNum === p ? "font-bold border-2 border-[#3A4884]" : ""}`}
+                                    key={p} onClick={() => cuisineNumClicked(p)}>{p}</button>)
+                        }
+                        {/* {
+                            selectedCuisineNum && selectedCuisineNum === 2 ? <div>This is great for an individual or a pair preparing their meals.</div> : selectedPeopleNum && selectedPeopleNum === 4 ? <div>This is great to for families or friends!</div> : null
+                        } */}
                     </div>
                     <div className="grid grid-cols-1 items-start px-4 tablet:grid-cols-2 web:grid-cols-4">
                         <div className="tablet:col-span-2 tablet:px-[130px] web:col-span-4 mt-8 web:mt-10 mb-2 web:px-[225px]">
@@ -231,21 +232,33 @@ const OurPlan: NextPage<{ cuisine: Cuisine[], goal: Goal[] }> = ({ cuisine, goal
                                 <div className={`flex flex-row gap-10 items-center justify-center`}>
                                     <span className={`text-sm`}>Number of people</span>
                                     <div>
-                                        <ChoiceButton onClick={handlePeopleClicked} choices={["2", "4"]} choiceTexts={["", ""]} height={"9"} />
+                                        {
+                                            peopleNum
+                                                .map((p) =>
+                                                    <button className={`bg-white border-2 w-32 rounded-sm text-[#3A4884] 
+                                                 ${selectedPeopleNum === p ? "font-bold border-2 border-[#3A4884]" : ""}`}
+                                                        key={p} onClick={() => peopleNumClicked(p)}>{p}</button>)
+                                        }
                                     </div>
                                 </div>
                                 <div className={`flex flex-row gap-10 items-center justify-center mt-5`}>
                                     <span className={`text-sm`}>Recipes per week</span>
                                     <div>
-                                        <ChoiceButton onClick={handleFoodButtonlicked} choices={["3", "4", "5"]} choiceTexts={["", "", ""]} height={"9"} />
+                                        {
+                                            cuisineNum
+                                                .map((p) =>
+                                                    <button className={`bg-white border-2 w-24 rounded-sm text-[#3A4884] 
+                                                    ${selectedCuisineNum === p ? "font-bold border-2 border-[#3A4884]" : ""}`}
+                                                        key={p} onClick={() => cuisineNumClicked(p)}>{p}</button>)
+                                        }
                                     </div>
                                 </div>
 
                                 <div className={`border-2 flex flex-col w-[99%] gap-5 mt-5 p-3`}>
                                     <div className={`flex flex-col  text-sm`}>
                                         <span className={`font-bold`}>Price Summary</span>
-                                        <span>{selectedOrder[1]} meals for {selectedOrder[0]} people per week</span>
-                                        <span>{selectedOrder[0] * selectedOrder[1]} total servings</span>
+                                        <span>{selectedCuisineNum} meals for {selectedPeopleNum} people per week</span>
+                                        <span>{selectedCuisineNum * selectedPeopleNum} total servings</span>
                                     </div>
                                     <div className={`flex flex-col text-sm`}>
                                         <div className={`flex flex-row w-full place-content-between`}>
@@ -294,9 +307,10 @@ const OurPlan: NextPage<{ cuisine: Cuisine[], goal: Goal[] }> = ({ cuisine, goal
                             cuisine
                                 .slice(0)
                                 .sort((a: any, b: any) => (a.index ?? 0) > (b.index ?? 0) ? 1 : -1)
-                                .map((c: any) => <button className={`bg-white border-2 h-[4rem] rounded-sm text-[#3A4884] 
-                            ${selectedCuisine.includes(c.id) ? "font-bold border-2 border-[#3A4884]" : ""}`}
-                                    key={c.id} onClick={() => cuisineClicked(c.id)}>{c.displayName}</button>)
+                                .map((c: any) =>
+                                    <button className={`bg-white border-2 h-[4rem] rounded-sm text-[#3A4884] 
+                                ${selectedCuisine.includes(c.id) ? "font-bold border-2 border-[#3A4884]" : ""}`}
+                                        key={c.id} onClick={() => cuisineClicked(c.id)}>{c.displayName}</button>)
                         }
                     </div>
                 </div>
